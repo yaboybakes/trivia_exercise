c5a0c797c7226a3571093e36493e5da8b64cf057
Justin S. Baiko <baiko@utexas.edu>
2016-09-03 22:06:28 -0500

    more logic

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ var game_over = false;
 var time_left = 30;
 var missed = 3;
 var flag = true;
+var wrong = 0;
 
 var questions = [
 {
@@ -26,10 +27,24 @@ $(document).ready(function() {
 
   start_game();
 
-  function start_game() {
-    console.log('start');
-    start_timer();
+  function refresh() {
+    console.log('refresh');
+    stop_timer();
+    console.log('index: ' + q_array_index);
+    question = questions[q_array_index].question;
+    answer = questions[q_array_index].correct;
+    console.log('new answer: '+ answer);
+    $(q0).text(question);
+    $(q1).text(questions[q_array_index].answers[0]);
+    $(q2).text(questions[q_array_index].answers[1]);
+    $(q3).text(questions[q_array_index].answers[2]);
+    $(q4).text(questions[q_array_index].answers[3]);
+    $(q5).text(questions[q_array_index].answers[4]);
+  }
 
+  function start_game() {
+
+    start_timer();
     var question = questions[q_array_index].question;
     var q0 = $(document).find("#q0");
     var q1 = $(document).find("#q1");
@@ -37,7 +52,8 @@ $(document).ready(function() {
     var q3 = $(document).find("#q3");
     var q4 = $(document).find("#q4");
     var q5 = $(document).find("#q5");
-    var answer = questions[q_array_index].correct;
+    answer = questions[q_array_index].correct;
+
 
     $(q0).text(question);
     $(q1).text(questions[q_array_index].answers[0]);
@@ -47,20 +63,29 @@ $(document).ready(function() {
     $(q5).text(questions[q_array_index].answers[4]);
 
     $('.trivia').click(function() {
-      console.log('click');
+
       missed--;
+      if (missed < 0) {
+        missed = 0;
+      }
+
       if (answer == 0) {
         answer = 1;
       }
+      console.log('answer: ' + answer);
       var ans = "q" + answer;
       var guess = $(this).attr('id');
-      console.log(ans);
-      console.log(guess);
+      console.log('ans: ' + ans);
+      console.log('guess:' + guess);
 
       if (guess == ans) {
         correct++;
         console.log('correct');
+      } else {
+        wrong++;
       }
+
+      refresh();
     });
 
 }
@@ -89,26 +114,30 @@ $(document).ready(function() {
   function stop_timer(){
 
       q_array_index++;
+      if (q_array_index > questions.length) {
+        q_array_index = 0;
+        correct = 0;
+      }
       time_left = 30;
 
       clearInterval(counter);
 
       if (q_array_index < questions.length) {
           start_timer();
-          start_game();
       } else {
           game_over();
       }
   }
 
   function game_over() {
-    var wrong = (questions.length - missed + correct);
+    $('.timer').empty();
     $('.trivia').empty();
     $('#q1').html('You got ' + correct + ' questions right.');
     $('#q2').html('You missed ' + missed + ' questions.') ;
     $('#q3').html('You got ' + wrong + ' questions wrong.');
     $('#q4').html('Replay?');
     $('#q5').html('<button id="yes">Yes</button><button id="no">NO</button>');
+
     $('#yes').click(function() {
       var q_array_index = 0;
       var correct = 0;
@@ -119,11 +148,12 @@ $(document).ready(function() {
       start_game();
     });
     $('#no').click(function() {
+      console.log('no');
       $('.trivia').empty();
+      $('.timer').html("");
       $('#q3').html('Game Over');
     });
   }
-
 
 
 });
